b7b63362367013bd497c4e712b1938238174c703
Kei Fukutani <kifinewera@gmail.com>
2021-01-25 20:46:18 -0800

    updated.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: "fsnd-iam-dev.us", // the auth0 domain prefix
-    audience: "coffeeshop", // the audience set for the auth0 app (API identifier)
-    clientId: "ziuQYXMvrn7uOdOWmlBNccHZkwTGxprU", // the client id generated for the auth0 app
+    url: "", // the auth0 domain prefix
+    audience: "", // the audience set for the auth0 app (API identifier)
+    clientId: "", // the client id generated for the auth0 app
     callbackURL: "http://127.0.0.1:8100", // the base url of the running ionic application.
   },
 };
